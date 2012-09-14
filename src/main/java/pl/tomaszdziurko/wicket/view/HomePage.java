d9b35803ea0321dc5e80f80d480a6af527a457fa
Tomasz Dziurko <tdziurko@gmail.com>
2012-09-14 16:15:10 +0200

    As a User I want to be able to logout and clear my cookies

--- src/main/java/pl/tomaszdziurko/wicket/view/HomePage.java
+++ src/main/java/pl/tomaszdziurko/wicket/view/HomePage.java
@@ -6,6 +6,9 @@ import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import pl.tomaszdziurko.wicket.UserSession;
+import pl.tomaszdziurko.wicket.WicketApplication;
+import pl.tomaszdziurko.wicket.service.CookieService;
+import pl.tomaszdziurko.wicket.service.SessionProvider;
 
 public class HomePage extends BasePage {
     private static final long serialVersionUID = 1L;
@@ -18,7 +21,12 @@ public class HomePage extends BasePage {
         Link<Void> logoutLink = new Link<Void>("logout") {
             @Override
             public void onClick() {
-                UserSession.get().invalidateNow();
+                CookieService cookieService = WicketApplication.get().getCookieService();
+                cookieService.removeCookieIfPresent(getRequest(), getResponse(), SessionProvider.REMEMBER_ME_LOGIN_COOKIE);
+                cookieService.removeCookieIfPresent(getRequest(), getResponse(), SessionProvider.REMEMBER_ME_PASSWORD_COOKIE);
+
+                UserSession.get().setUser(null);
+                UserSession.get().invalidate();
             }
         };
         logoutLink.setVisible(UserSession.get().userLoggedIn());
